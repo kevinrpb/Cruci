ec033d31843bbcb9a67f599e90c1c537d2c7efef
Kevin Romero Peces-Barba <kevinrpb@hotmail.com>
2022-03-23 08:41:19 +0100

    Initial Commit

---
+++ Cruci/Application/CruciApp.swift
@@ -0,0 +1,17 @@
+//
+//  CruciApp.swift
+//  Cruci
+//
+//  Created by Kevin Romero Peces-Barba on 23/3/22.
+//
+
+import SwiftUI
+
+@main
+struct CruciApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cruci/Views/PKCanvas.swift
@@ -0,0 +1,8 @@
+//
+//  PKCanvas.swift
+//  Cruci
+//
+//  Created by Kevin Romero Peces-Barba on 23/3/22.
+//
+
+import Foundation
