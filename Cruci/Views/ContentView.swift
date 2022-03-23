175bc8b66eb8c681709473f9bf4c6688d3c9e4a5
Kevin Romero Peces-Barba <kevinrpb@hotmail.com>
2022-03-23 10:49:36 +0100

    PencilKit support MVP

--- Cruci/Views/ContentView.swift
+++ Cruci/Views/ContentView.swift
@@ -6,11 +6,96 @@
 //
 
 import SwiftUI
+import PencilKit
+
+enum Tool {
+    case ink, eraser
+}
 
 struct ContentView: View {
+    @Environment(\.undoManager) var undoManager
+
+    @State var listItemDrawing: Data? = nil
+    @State var drawingData: Data? = nil
+
+    @State var tool: Tool = .ink
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                Section {
+                    ZStack {
+                        Text("1. This is some long text that could simulate a clue (4)")
+                        PKCanvas(data: $listItemDrawing, tool: $tool)
+                    }
+                } header: {
+                    Text("Across")
+                }
+            }
+            .navigationTitle("Clues")
+            .navigationBarTitleDisplayMode(.inline)
+
+            ZStack {
+                PKCanvas(data: $drawingData, tool: $tool)
+            }
+            .toolbar {
+                ToolbarItemGroup(placement: .bottomBar) {
+                    UndoButton()
+                    RedoButton()
+                }
+                ToolbarItemGroup(placement: .status) {
+                    SelectInkToolButton()
+                    SelectEraserToolButton()
+                }
+            }
+            .navigationTitle("Drawing")
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarHidden(true)
+        }
+    }
+
+    private func SelectInkToolButton() -> some View {
+        Button {
+            tool = .ink
+        } label: {
+            Label("Ink", systemImage: "pencil")
+                .padding(4)
+                .background(
+                    RoundedRectangle(cornerRadius: 6, style: .continuous)
+                        .fill(tool == .ink ? .blue.opacity(0.2) : .clear)
+                )
+        }
+    }
+
+    private func SelectEraserToolButton() -> some View {
+        Button {
+            tool = .eraser
+        } label: {
+            Label("Eraser", systemImage: "square.dashed")
+                .padding(4)
+                .background(
+                    RoundedRectangle(cornerRadius: 6, style: .continuous)
+                        .fill(tool == .eraser ? .blue.opacity(0.2) : .clear)
+                )
+        }
+    }
+
+    private func UndoButton() -> some View {
+        Button {
+            undoManager?.undo()
+        } label: {
+            Label("Undo", systemImage: "arrow.uturn.backward")
+        }
+//        .disabled(!(undoManager?.canUndo ?? false))
+    }
+
+    private func RedoButton() -> some View {
+        Button {
+            undoManager?.redo()
+        } label: {
+            Label("Undo", systemImage: "arrow.uturn.forward")
+        }
+//        .disabled(!(undoManager?.canRedo ?? false))
     }
 }
 
--- Cruci/Views/PKCanvas.swift
+++ Cruci/Views/PKCanvas.swift
@@ -5,4 +5,58 @@
 //  Created by Kevin Romero Peces-Barba on 23/3/22.
 //
 
-import Foundation
+import SwiftUI
+import PencilKit
+
+struct PKCanvas: UIViewRepresentable {
+    @Binding var data: Data?
+    @Binding var tool: Tool
+
+    private var canvasView: PKCanvasView
+
+    private let inkingTool: PKInkingTool = .init(.pen, color: .blue, width: 15)
+    private let eraserTool: PKEraserTool = .init(.vector)
+
+    public init(data: Binding<Data?>, tool: Binding<Tool>) {
+        self._data = data
+        self._tool = tool
+
+        canvasView = .init()
+    }
+
+    func makeUIView(context: Context) -> PKCanvasView {
+        canvasView.tool = tool == .ink ? inkingTool : eraserTool
+        canvasView.isOpaque = false
+        canvasView.isScrollEnabled = false
+        canvasView.drawingPolicy = .anyInput
+
+        canvasView.becomeFirstResponder()
+        canvasView.delegate = context.coordinator
+
+        if let data = data, let drawing = try? PKDrawing(data: data) {
+            canvasView.drawing = drawing
+        }
+
+        return canvasView
+    }
+
+    func updateUIView(_ canvasView: PKCanvasView, context: Context) {
+        canvasView.tool = self.tool == .ink ? inkingTool : eraserTool
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, PKCanvasViewDelegate {
+        var canvas: PKCanvas
+
+        init(_ canvas: PKCanvas) {
+            self.canvas = canvas
+        }
+
+        func canvasViewDrawingDidChange(_ canvasView: PKCanvasView) {
+            canvas.data = canvasView.drawing.dataRepresentation()
+        }
+    }
+}
